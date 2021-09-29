386877f3dd09e434c58473d0c8bf383fb3ad5341
Iván Pereira Muñoz <elchininet@gmail.com>
2021-09-29 19:09:14 +0200

    Refactor the fetchDb utility

--- src/js/services/ApiServices.js
+++ src/js/services/ApiServices.js
@@ -1,27 +1,48 @@
    
-const SCHEDULED_SESSIONS_SERVER = 'http://localhost:3001/scheduledSessions';
-const SESSIONS_SERVER = 'http://localhost:3001/sessions';
-const USERS_SERVER = 'http://localhost:3001/users';
+const HOST = 'http://localhost:3001';
+const SCHEDULED_SESSIONS_SERVER = `${HOST}/scheduledSessions`;
+const SESSIONS_SERVER = `${HOST}/sessions`;
+const USERS_SERVER = `${HOST}/users`;
 // const SERVER_SHEDULED_SESSIONS = `${SCHEDULED_SESSIONS_SERVER}`;
 // const SERVER_SESSIONS = `${SERVER}/sessions`;
 // const SERVER_USERS = `${SERVER}/users`;
 
+const METHODS = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    DELETE: 'DELETE'
+};
+
 const fetchDb = (endpoint, method, data) => {
-    const options = {
-        method,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-    if (data) options.body = JSON.stringify(data);
+    
+    const options = { method };
+
+    if (data) {
+        if (method === METHODS.GET) {
+            const params = new URLSearchParams(data);
+            endpoint += `?${params.toString()}`;
+        } else {
+            options.headers = {
+                'Content-Type': 'application/json'
+            };
+            options.body = JSON.stringify(data);
+        }        
+    }
 
     return new Promise((resolve, reject) => {
         fetch(endpoint, options)
             .then(response => {
                 if (response.ok) {
-                    response
-                        .json()
-                        .then(json => resolve(json));
+                    if (method === METHODS.GET) {
+                        response
+                            .json()
+                            .then(json => resolve(json));
+                    } else {
+                        response
+                            .text()
+                            .then(text => resolve(text));
+                    }
                 } else {
                     reject('Server error');
                 }
@@ -34,19 +55,32 @@ const fetchDb = (endpoint, method, data) => {
 class ApiServices {
     // Load scheduled sessions
     loadScheduledSessions() {
-        return fetchDb(SCHEDULED_SESSIONS_SERVER, 'GET');
+        return fetchDb(
+            SCHEDULED_SESSIONS_SERVER,
+            METHODS.GET
+        );
     }
     // Load sessions
     loadSessions() {
-        return fetchDb(SESSIONS_SERVER, 'GET');
+        return fetchDb(
+            SESSIONS_SERVER,
+            METHODS.GET
+        );
     }
     // Delete session
     deleteScheduledSession(sessionID) {
-        return fetchDb(`${SCHEDULED_SESSIONS_SERVER}/${sessionID}`, 'DELETE');
+        return fetchDb(
+            `${SCHEDULED_SESSIONS_SERVER}/${sessionID}`,
+            METHODS.DELETE
+        );
     }
     // Update scheduled sessions rounds definition
     updateRoundsDefinition(sessionID) {
-        return fetchDb(`${SCHEDULED_SESSIONS_SERVER}/${sessionID}`, 'PUT', {roundsDefinition});
+        return fetchDb(
+            `${SCHEDULED_SESSIONS_SERVER}/${sessionID}`,
+            METHODS.PUT,
+            {roundsDefinition}
+        );
     }
 }
 
